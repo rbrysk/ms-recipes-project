c837c5add93b9a26c1c3fa7aa001697249fa0fe2
rbrysk <bryskiewiczr@protonmail.com>
2024-02-28 23:24:30 +0100

    add page switching via event listeners

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Output, OnInit, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+    @Output()
+    switchPage = new EventEmitter<string>();
+
+    onSelect(page: string) {
+        this.switchPage.emit(page);
+    }
+
+}
