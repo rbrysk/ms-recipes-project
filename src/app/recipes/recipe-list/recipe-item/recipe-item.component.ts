6723c9d6344d742ec16bc030e0e2a1ad52161719
rbrysk <bryskiewiczr@protonmail.com>
2024-03-01 23:37:11 +0100

    feature added

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from '../../recipe.model';
 
@@ -12,7 +12,15 @@ export class RecipeItemComponent implements OnInit {
     @Input()
     recipe: Recipe;
 
+    @Output()
+    onRecipeSelected = new EventEmitter<void>();
+
     constructor() { }
 
     ngOnInit() { }
+
+    selectRecipe(selectedRecipe: Recipe) {
+        this.onRecipeSelected.emit();
+    }
+
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,6 +8,10 @@ import { Recipe } from '../recipe.model';
     styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent implements OnInit {
+    
+    @Output()
+    recipeWasSelected = new EventEmitter<Recipe>();
+    
     recipes: Recipe[] = [
         new Recipe('A Test Recipe', 'This is simply a test', 'https://assets.epicurious.com/photos/5877a9262ee1d51f664035fb/4:3/w_1724,h_1293,c_limit/Miso-Tahini-Squash-Soup-With-Brown-Rice-recipe-12012017.jpg'),
         new Recipe('A Second Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1cF_5kfxl6XNY8lEBKJuXpN544kCjKidvz9-vri34Hw&s')
@@ -16,4 +20,8 @@ export class RecipeListComponent implements OnInit {
     constructor() {}
 
     ngOnInit(): void {}
+
+    recipeSelected(recipe: Recipe){
+        this.recipeWasSelected.emit(recipe);
+    }
 }
