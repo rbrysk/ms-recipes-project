198376e315a4e178b8e2429c43751056ce521721
rbrysk <bryskiewiczr@protonmail.com>
2024-03-02 20:44:24 +0100

    add adding ingredients to shopping list

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
     selector: 'app-shopping-list-edit',
     templateUrl: './shopping-list-edit.component.html',
     styleUrl: './shopping-list-edit.component.css',
 })
-export class ShoppingListEditComponent {}
+export class ShoppingListEditComponent {
+
+    @ViewChild('nameInput')
+    nameInputReference: ElementRef;
+
+    @ViewChild('amountInput')
+    amountInputReference: ElementRef;
+
+    @Output()
+    ingredientAddedEvent = new EventEmitter<Ingredient>();
+
+    addItem() {
+        const ingredientName = this.nameInputReference.nativeElement.value;
+        const ingredientAmount = this.amountInputReference.nativeElement.value;
+        const newIngredient: Ingredient = new Ingredient(ingredientName, ingredientAmount);
+        this.ingredientAddedEvent.emit(newIngredient);
+        console.log(newIngredient)
+    }
+}
